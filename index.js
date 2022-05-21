4d31145233166bb22c657c90110af6b6484d81f8
JB Paguyo <janbrian.paguyo@play.co>
2022-05-21 12:32:45 -0700

    calculate input frame rate from number of frames over elapsed time.

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const PuppeteerMassScreenshots = require('puppeteer-mass-screenshots');
 
 class PuppeteerVideoRecorder {
     config = {
-        inputFrameRate: 34,
+        inputFrameRate: -1,
         outputFrameRate: 60,
         ffmpegPath: 'ffmpeg',
     };
+    recordStartTime = -1;
+    recordEndTime = -1;
+    frames = 0;
     constructor(config){
         this.config = Object.assign({}, this.config, config || {});
         this.screenshots = new PuppeteerMassScreenshots();
@@ -19,28 +22,40 @@ class PuppeteerVideoRecorder {
         if (!fileName) {
             fileName = Date.now() + '.webm';
         }
+        this.frames = 0;
+        this.recordStartTime = -1;
+        this.recordEndTime = -1;
         this.page = page;
         this.outputFolder = resolve(process.cwd(), outputFolder);
         await this.fsHandler.init(this.outputFolder, fileName);
         const { imagesPath,imagesFilename, appendToFile } = this.fsHandler;
         await this.screenshots.init(page, imagesPath, {
-            afterWritingImageFile: (filename) => appendToFile(imagesFilename, `file '${filename}'\n`)
+            afterWritingImageFile: (filename) => {
+                appendToFile(imagesFilename, `file '${filename}'\n`);
+                this.frames += 1;
+            }
         });
     }             
 
     start(options = {}) { 
+        this.recordStartTime = Date.now();
         return this.screenshots.start(options);
     }
     
     async stop () {
     	await this.screenshots.stop();
+        this.recordEndTime = Date.now();
     }
 
     get defaultFFMpegCommand() {
         const { imagesFilename, videoFilename } = this.fsHandler;
+        // calculate inputFrameRate from frames over elapsed time if needed
+        const inputFrameRate = this.config.inputFrameRate === -1 ?
+            this.frames / ((this.recordEndTime - this.recordStartTime) / 1000) :
+            this.config.inputFrameRate;
         return [
             this.config.ffmpegPath,
-            `-r ${this.config.inputFrameRate}`,
+            `-r ${inputFrameRate}`,
             '-f concat',
             '-safe 0',
             `-i "${imagesFilename}"`,
